d4a94ab193165e841bf5c53f0e20ebe17b9d8ecc
barick <barick3018@gmail.com>
2025-06-11 14:35:18 +0530

    adam

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ def main():
     print(f"Secret username: {secret_user}")
     print(f"Secret password: {secret_pass}")
 
+    while True:
+        time.sleep(60)
+
 if __name__ == "__main__":
     main()
